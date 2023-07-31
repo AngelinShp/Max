0e65c1f905ae8dab87256c3ba1b2bdb5acf27045
AngelinShp <angelinashpspb@gmail.com>
2023-07-31 18:19:32 +0300

    Admin

--- advertisements/app_adv/admin.py
+++ advertisements/app_adv/admin.py
@@ -6,7 +6,16 @@ class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'price',
                     'created_time', 'auction']
     list_filter = ['auction', 'created_time']
+
     actions=['make_auction_false', 'make_auction_true']
+    fieldsets = (
+        (
+            'Общее', {'fields': ('title', 'description')}
+        ),
+        (
+            'Финансы', {'fields': ('price', 'auction')}
+        )
+    )
     @admin.action(description='Убрать возможность торга')
     def make_auction_false(self, request, queryset):
         queryset.update(auction=False)
