0791ff59979686f39846bccdb6822cdc33ade9a1
AngelinShp <angelinashpspb@gmail.com>
2023-07-31 18:10:12 +0300

    Admin

--- advertisements/app_adv/admin.py
+++ advertisements/app_adv/admin.py
@@ -3,6 +3,15 @@ from .models import Advertisements
 # Register your models here.
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'description', 'price',
+                    'created_time', 'auction']
+    list_filter = ['auction', 'created_time']
+    actions=['make_auction_false', 'make_auction_true']
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_false(self, request, queryset):
+        queryset.update(auction=False)
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_true(self, request, queryset):
+        queryset.update(auction=True)
 
 admin.site.register(Advertisements, AdvertisementAdmin)
