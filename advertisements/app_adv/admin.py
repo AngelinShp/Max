883965292c6730d9d2d1ec80c685720eb038dd43
AngelinShp <angelinashpspb@gmail.com>
2023-07-31 17:50:24 +0300

    Admin

--- advertisements/app_adv/admin.py
+++ advertisements/app_adv/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Advertisements
 # Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Advertisements, AdvertisementAdmin)
--- advertisements/app_lesson_4/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-# Create your views here.
-def homework(request):
-    return HttpResponse('Домашка по 4 занятию')
-
-
