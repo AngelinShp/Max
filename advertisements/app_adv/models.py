aee144326d9e2d84b275f6b4aeeffee6af110fcd
AngelinShp <angelinashpspb@gmail.com>
2023-08-14 16:56:42 +0300

    ДЗ-9_Наполнение_шаблонов

--- advertisements/app_adv/models.py
+++ advertisements/app_adv/models.py
@@ -37,6 +37,12 @@ class Advertisements(models.Model):
                                '{}</span>', upd_time)
         return self.updated_time.strftime("%d.%m.%Y в %H:%M:%S")
 
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;">', url=self.image.url)
+
     # def __str__(self):
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
